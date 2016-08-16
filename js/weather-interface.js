4fb1336e818fd2c137dad3c2dd290dd69d5109c4
Andrew Niekamp and Russ Davies <andrew+russ@>
2016-08-16 16:10:47 -0700

    refactor and add fahrenheit to the api string

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,15 @@
-var apiKey = "770d4bde064f6f0e0b75987d411d76c9";
+var Weather = require('./../js/weather.js').weatherModule;
+var Temperature = require('./../js/weather.js')
+
+var displayWeather = function(city, humidityData, tempData) {
+    $('.showWeather').html("<p>The humidity in " + city + " is " + humidityData + "%.</p><p>The temperature is " + tempData + " degrees.");
+}
 
 $(document).ready(function() {
-  $("#weatherLocation").click(function() {
-    var city = $("#location").val();
-    $("#location").val("");
-    $(".showWeather").text("The city you have chosen is " + city + ".");
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayWeather);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
+var apiKey = require('./../.env').apiKey;
 
-});
+function Weather() {
+}
+
+Weather.prototype.getWeather = function(city, displayFunction) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + "&units=imperial").then(function(response) {
+    displayFunction(city, response.main.humidity, response.main.temp);
+  }).fail(function(error) {
+    $('.showWeather').text(error.responseJSON.message);
+  });
+}
+
+exports.weatherModule = Weather;
